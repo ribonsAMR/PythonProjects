c6f0b25045f9336da7fc3bda6eb88f339f13c003
Amr Essam <amressam01@hotmail.com>
2019-01-29 11:29:21 +0200

    Format the file
    Add raw notation to the pattern string

--- 100/madlib/madlib.py
+++ 100/madlib/madlib.py
@@ -4,8 +4,7 @@ init(autoreset=True)
 
 # This pattern finds <noun> and <#noun>
 
-general_pattern = "<#?\w+>"
-
+general_pattern = r"<#?\w+>"
 
 # Hashtage sign (#) indicates that this variable equals the previous variable.
 
@@ -15,7 +14,6 @@ But what I do have are a very particular set of <plural>, <#plural> I have acqui
 <#plural> that make me a <noun> for people like you. If you let my <noun> go now, that'll be the end of it.
 I will not <verb> for you, I will not pursue you.
 But if you don't, I will <#verb> for you, I will find you, and I will <verb> you."""  # NOQA
-
 """
 def cleanup(x):
     return x.replace('<', '').replace('>', '').replace('#', '')
@@ -23,41 +21,42 @@ def cleanup(x):
 
 
 def main():
-    global general_pattern, text
+  global general_pattern, text
 
-    # lambda function to remove <, > and # from the string to present it.
+  # lambda function to remove <, > and # from the string to present it.
 
-    def cleanup(x): return x.replace('<', '').replace('>', '').replace('#', '')
+  def cleanup(x):
+    return x.replace('<', '').replace('>', '').replace('#', '')
 
-    # Get the variables list of the text
+  # Get the variables list of the text
 
-    replace_list = re.findall(general_pattern, text)
+  replace_list = re.findall(general_pattern, text)
 
-    for index, value in enumerate(replace_list):
+  for index, value in enumerate(replace_list):
 
-        # If it starts with #, follow the last input and continue
+    # If it starts with #, follow the last input and continue
 
-        if value.startswith("<#"):
+    if value.startswith("<#"):
 
-            replace_list[index] = replace_list[index - 1]
-            text = text.replace(value, replace_list[index], 1)
-            continue
+      replace_list[index] = replace_list[index - 1]
+      text = text.replace(value, replace_list[index], 1)
+      continue
 
-        # Else:
+    # Else:
 
-        while True:
-            user_input = input("Enter {}: ".format(cleanup(value)))
+    while True:
+      user_input = input("Enter {}: ".format(cleanup(value)))
 
-            if not user_input:
-                continue
+      if not user_input:
+        continue
 
-            replace_list[index] = Fore.MAGENTA + user_input + Fore.RESET
-            text = text.replace(value, replace_list[index], 1)
+      replace_list[index] = Fore.MAGENTA + user_input + Fore.RESET
+      text = text.replace(value, replace_list[index], 1)
 
-            break
+      break
 
-    print("\nThe final text is:\n\n" + text + "\n")
+  print("\nThe final text is:\n\n" + text + "\n")
 
 
 if __name__ == '__main__':
-    main()
+  main()
